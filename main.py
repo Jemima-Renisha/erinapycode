1d82eab7b687a0158d8e68367d2af86d14f1b552
Jemima-Renisha <renishareni08@gmail.com>
2024-09-04 22:09:37 +0530

    rockpapergame

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print("Hello welcome!")
+import random
+choices = ['rock', 'paper', 'scissors']
+
+def getcomputerchoice():
+    return random.choice(choices)
+
+def playerchoice():
+    print("choose rock or paper or scissors")
+    playerinput = input("your choice:")
+    while playerinput not in choices:
+        print("Invalid choice")
+        playerinput = input("your choice:")
+    return playerinput
+
+def findwinner(playerinput, computerinput):
+    if playerinput == computerinput:
+        return "it is tie"
+    elif (playerinput=="rock" and computerinput=="scissors")or (playerinput=="paper" and computerinput=="rock") or(playerinput=="scissors" and computerinput=="paper"):
+        return "you win"
+    else:
+        return "computer win"
